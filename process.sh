2c51c99e48268b1aa2c4e4abcb0881da658be90e
Sacha Wheeler <sacha@sachawheeler.com>
2016-12-01 10:52:13 +0000

    add shell script

---
+++ process.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# erica
+
+# 1. https://www.youtube.com/watch?v=Z-8qS-fdBy4
+# (only need 00:16 – 00:30)
+# youtube-dl -f 18 https://www.youtube.com/watch?v=Z-8qS-fdBy4
+ 
+
+# 2. https://www.youtube.com/watch?v=LgQscL-ialg
+# (only need from start to 1:40)
+youtube-dl -f 18 https://www.youtube.com/watch?v=LgQscL-ialg
+ 
+# 3. https://www.youtube.com/watch?v=v92tWHSUAr
+# (only need from start to 1:40)
+youtube-dl -F https://www.youtube.com/watch?v=v92tWHSUAr
+
+# 4. https://www.youtube.com/watch?v=wkqz6j8ZusA
+# (only need from 1:32 - 2:20)
+youtube-dl -f 18 https://www.youtube.com/watch?v=wkqz6j8ZusA
+
+# 5. https://www.youtube.com/watch?v=nxo84aJJvWc
+# (only need from 2:53 - 3:14)
+youtube-dl -f 18 https://www.youtube.com/watch?v=nxo84aJJvWc
+
+# 6. https://www.youtube.com/watch?v=ObHSXfh8p7U
+# (only need from 5:46 - 6:10)
+youtube-dl -f 18 https://www.youtube.com/watch?v=ObHSXfh8p7U
+
+# 7. https://www.youtube.com/watch?v=HA8gY59pueg
+# (only need 32:34 - 33:55)
+youtube-dl -f 22 https://www.youtube.com/watch?v=HA8gY59pueg
+
+# 8. https://www.youtube.com/watch?v=xUyYtcv8zhQ
+# (only need :18 - :30 and 1:53 - 1:58)
+youtube-dl -f 22 https://www.youtube.com/watch?v=xUyYtcv8zhQ
