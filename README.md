af1b153abfaf7d4300a4a1025b5f119cdc95fe57
Sacha Wheeler <sacha@sachawheeler.com>
2016-12-05 22:26:06 +0000

    and the readme

--- README.md
+++ README.md
@@ -31,3 +31,21 @@ result https://github.com/game0ver/erica/raw/master/HA8gY59pueg.mp4
 8. https://www.youtube.com/watch?v=xUyYtcv8zhQ
 (only need :18 - :30 and 1:53 - 1:58) 
 results https://github.com/game0ver/erica/raw/master/xUyYtcv8zhQ.mp4 https://github.com/game0ver/erica/raw/master/xUyYtcv8zhQ-2.mp4
+
+--------
+
+
+https://www.youtube.com/watch?v=8H0tFBBsQtI
+(from 4:25 - 5:00... sleeping in space)
+
+https://www.youtube.com/watch?v=JSbj2EJnrJc
+(whole thing, reagan clip)
+
+https://www.youtube.com/watch?v=wumXDthpNZM
+(from :32 - end)
+
+https://www.youtube.com/watch?v=8t2KNqJdHXo&index=10&list=PL6L-AyuXibhb25weD-ztKmwoGt8INdkkf (and around 16:46 - 17:18 and 19:35 - 20:48)
+
+https://www.youtube.com/watch?v=sebIaAiwfqU
+(4:05 - 5:10)
+
--- edit2.sh
+++ edit2.sh
@@ -11,12 +11,12 @@
 # ffmpeg -i "1983 - Reagan honors Sally Ride, astronauts-JSbj2EJnrJc.mp4"\
     # -ss 00:00:00 -t00:00:00:00 -async 1 -c copy JSbj2EJnrJc.mp4
 ffmpeg -i "Live Footage June 18, 1983 Space Shuttle Challenger Mission Taken On-board Cameras - imasportsphile-sebIaAiwfqU.mp4"\
-    -ss 00:00:00 -t 00:00:00 -async 1 -c copy sebIaAiwfqU.mp4
-ffmpeg -i "Living and Working in Space  NASA 1983 circa-8H0tFBBsQtI.mp4"\
-    -ss 00:04:25 -t 00:00:35 -async 1 -c copy 8H0tFBBsQtI.mp4
-ffmpeg -i "Sally Ride 'Not Hard' To Be US Space Pioneer-wumXDthpNZM.mp4"\
-    -ss 00:00:32 -async 1 -c copy wumXDthpNZM.mp4
-ffmpeg -i "Space Shuttle - 'MISSION TO THE FUTURE' 1981 NASA Documentary - WDTVLIVE42-8t2KNqJdHXo.mp4"\
-    -ss 00:16:46 -t 00:00:32 -async 1 -c copy 8t2KNqJdHXo.mp4
-ffmpeg -i "Space Shuttle - 'MISSION TO THE FUTURE' 1981 NASA Documentary - WDTVLIVE42-8t2KNqJdHXo.mp4"\
-    -ss 00:19:35 -t 00:01:13 -async 1 -c copy 8t2KNqJdHXo-2.mp4
+    -ss 00:04:05 -t 00:01:05 -async 1 -c copy sebIaAiwfqU.mp4
+# ffmpeg -i "Living and Working in Space  NASA 1983 circa-8H0tFBBsQtI.mp4"\
+    # -ss 00:04:25 -t 00:00:35 -async 1 -c copy 8H0tFBBsQtI.mp4
+# ffmpeg -i "Sally Ride 'Not Hard' To Be US Space Pioneer-wumXDthpNZM.mp4"\
+    # -ss 00:00:32 -async 1 -c copy wumXDthpNZM.mp4
+# ffmpeg -i "Space Shuttle - 'MISSION TO THE FUTURE' 1981 NASA Documentary - WDTVLIVE42-8t2KNqJdHXo.mp4"\
+    # -ss 00:16:46 -t 00:00:32 -async 1 -c copy 8t2KNqJdHXo.mp4
+# ffmpeg -i "Space Shuttle - 'MISSION TO THE FUTURE' 1981 NASA Documentary - WDTVLIVE42-8t2KNqJdHXo.mp4"\
+    # -ss 00:19:35 -t 00:01:13 -async 1 -c copy 8t2KNqJdHXo-2.mp4
