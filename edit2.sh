f449b52cb96ca78a4b5e66ec55400c2c57f47606
Sacha Wheeler <sacha@sachawheeler.com>
2016-12-05 22:18:08 +0000

    seconsd batch

---
+++ edit2.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# erica
+
+# 1. https://www.youtube.com/watch?v=Z-8qS-fdBy4
+# (only need 00:16 – 00:30)
+# youtube-dl -f 18 https://www.youtube.com/watch?v=Z-8qS-fdBy4
+# ffmpeg -i "Within this Decade - America in Space - 1969-Z-8qS-fdBy4.mp4"\
+# 1983 - Reagan honors Sally Ride, astronauts-JSbj2EJnrJc.mp4
+    # -ss 00:00:16 -t 00:00:14 -async 1 -c copy Z-8qS-fdBy4.mp4
+
+# ffmpeg -i "1983 - Reagan honors Sally Ride, astronauts-JSbj2EJnrJc.mp4"\
+    # -ss 00:00:00 -t00:00:00:00 -async 1 -c copy JSbj2EJnrJc.mp4
+ffmpeg -i "Live Footage June 18, 1983 Space Shuttle Challenger Mission Taken On-board Cameras - imasportsphile-sebIaAiwfqU.mp4"\
+    -ss 00:00:00 -t 00:00:00 -async 1 -c copy sebIaAiwfqU.mp4
+ffmpeg -i "Living and Working in Space  NASA 1983 circa-8H0tFBBsQtI.mp4"\
+    -ss 00:04:25 -t 00:00:35 -async 1 -c copy 8H0tFBBsQtI.mp4
+ffmpeg -i "Sally Ride 'Not Hard' To Be US Space Pioneer-wumXDthpNZM.mp4"\
+    -ss 00:00:32 -async 1 -c copy wumXDthpNZM.mp4
+ffmpeg -i "Space Shuttle - 'MISSION TO THE FUTURE' 1981 NASA Documentary - WDTVLIVE42-8t2KNqJdHXo.mp4"\
+    -ss 00:16:46 -t 00:00:32 -async 1 -c copy 8t2KNqJdHXo.mp4
+ffmpeg -i "Space Shuttle - 'MISSION TO THE FUTURE' 1981 NASA Documentary - WDTVLIVE42-8t2KNqJdHXo.mp4"\
+    -ss 00:19:35 -t 00:01:13 -async 1 -c copy 8t2KNqJdHXo-2.mp4
---
+++ process2.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# erica
+
+# 1. https://www.youtube.com/watch?v=Z-8qS-fdBy4
+# (only need 00:16 – 00:30)
+# youtube-dl -f 18 https://www.youtube.com/watch?v=Z-8qS-fdBy4
+ 
+
+# 2. https://www.youtube.com/watch?v=LgQscL-ialg
+# (only need from start to 1:40)
+# youtube-dl -f 18 https://www.youtube.com/watch?v=LgQscL-ialg
+ 
+# 3. https://www.youtube.com/watch?v=v92tWHSUAr
+# (only need from start to 1:40)
+# youtube-dl -F https://www.youtube.com/watch?v=v92tWHSUAr
+
+# 4. https://www.youtube.com/watch?v=wkqz6j8ZusA
+# (only need from 1:32 - 2:20)
+# youtube-dl -f 18 https://www.youtube.com/watch?v=wkqz6j8ZusA
+
+# 5. https://www.youtube.com/watch?v=nxo84aJJvWc
+# (only need from 2:53 - 3:14)
+# youtube-dl -f 18 https://www.youtube.com/watch?v=nxo84aJJvWc
+
+# 6. https://www.youtube.com/watch?v=ObHSXfh8p7U
+# (only need from 5:46 - 6:10)
+# youtube-dl -f 18 https://www.youtube.com/watch?v=ObHSXfh8p7U
+
+# 7. https://www.youtube.com/watch?v=HA8gY59pueg
+# (only need 32:34 - 33:55)
+# youtube-dl -f 22 https://www.youtube.com/watch?v=HA8gY59pueg
+
+# 8. https://www.youtube.com/watch?v=xUyYtcv8zhQ
+# (only need :18 - :30 and 1:53 - 1:58)
+# youtube-dl -f 22 https://www.youtube.com/watch?v=xUyYtcv8zhQ
+
+youtube-dl -f 18 https://www.youtube.com/watch?v=8H0tFBBsQtI
+# (from 4:25 - 5:00... sleeping in space)
+
+youtube-dl -f 18 https://www.youtube.com/watch?v=JSbj2EJnrJc
+# (whole thing, reagan clip)
+
+youtube-dl -f 18 https://www.youtube.com/watch?v=wumXDthpNZM
+# (from :32 - end)
+
+youtube-dl -f 22 https://www.youtube.com/watch?v=8t2KNqJdHXo
+# (and around 16:46 - 17:18 and 19:35 - 20:48)
+
+youtube-dl -f 18 https://www.youtube.com/watch?v=sebIaAiwfqU
+# (4:05 - 5:10)
+
